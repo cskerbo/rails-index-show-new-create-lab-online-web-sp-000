03f27e4a41c6e4df7f6559a09d534146fa0f6b9a
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-11 09:43:29 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,5 +4,17 @@ class CouponsController < ApplicationController
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
     @coupon.save
+  end
+
+  def new
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def index
+    @coupons = Coupon.all
+  end
 
 end
