6048f60d93a767bc5d30d9634dc1a6ad2bed15db
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-11 09:42:36 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,8 @@
 class CouponsController < ApplicationController
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
 
 end
