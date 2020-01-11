3a6b442010d3ecc8d899b8b283fc2413e687c93b
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-11 10:32:43 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,7 +4,7 @@ class CouponsController < ApplicationController
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
     @coupon.save
-    redirect_to coupons_path(@coupon)
+    redirect_to coupons_show_path(@coupon)
   end
 
   def new
