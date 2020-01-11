6f41b7e661c5e41c14eff39ff97e2af4c41b4e3a
Chris Kerbo <chris.kerbo@outlook.com>
2020-01-11 09:49:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_coupons.rb
+++ db/migrate/001_create_coupons.rb
@@ -1,7 +1,7 @@
 class CreateCoupons < ActiveRecord::Migration
   def change
     create_table :coupons do |c|
-      c.integer :coupon_code
+      c.string :coupon_code
       c.string :store
     end
   en
